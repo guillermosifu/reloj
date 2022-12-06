adcb9f36918dabf5da3f9182dcaf61266304abe5
guillermosifu <sifuentes1201@gmail.com>
2022-12-05 22:40:19 -0500

    cot

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const months =[
+const months = [
     'Enero',
     'Febrero',
     'Marzo',
@@ -11,9 +11,8 @@ const months =[
     'Octubre',
     'Noviembre',
     'Diciembre',
-];
-
-const weekdays=[
+  ];
+  const weekdays = [
     'Domingo',
     'Lunes',
     'Martes',
@@ -21,56 +20,70 @@ const weekdays=[
     'Jueves',
     'Viernes',
     'Sabado',
-];
-
-const calendario = document.querySelector('.calendario');
-const deadline = document.querySelector('.deadline');
-const items = document.querySelectorAll('.deadline-format h4');
-
-let tempDate = new Date();
-let tempYear = tempDate.getFullYear();
-let tempMonth = tempDate.getMonth();
-let tempDay = tempDate.getDate();
-
-const futureDate = new Date (tempYear,tempMonth,tempDay + 17,19,0,0)
-
-const year = futureDate.getFullYear();
-const hours = futureDate.getHours();
-const minutes = futureDate.getMinutes();
-
-let month = futureDate.getMonth();
-month = months[month]
-const weekday= weekdays[futureDate.getDay()];
-const date = futureDate.getDate();
-
-calendario.textContent = `El tiempo termina  ${weekday},${date} ${month} ${year} ${hours}: ${minutes}`
-
-const futureTime = futureDate.getTime();
-function getRemaindingTime()
-const today = new Date().getTime();
-
-const t = futureTime - today;
-//1 = 1000 ms
-//1 minut = 60s
-//1hr = 60m
-//1 = 24 hr
-// en milisegundos
-
-const oneDay = 24 * 60 * 60 * 1000;
-const oneHour = 60 * 60 * 1000;
-const oneMinute = 60 * 1000;
-
-let days = t /oneDay;
-days = Math.floor(days);
-let hour = Math.floor((t % oneDay)/oneHour);
-let minute = Math.floor((t % oneHour)/oneMinute);
-let seconds = Math.floor((t % oneMinute)/1000);
-
-
-const value = [days,hour,minutes,seconds]
-
-function format (item){
-    
-
-}
-
+  ];
+  const calendario = document.querySelector('.calendario');
+  const deadline = document.querySelector('.deadline');
+  const items = document.querySelectorAll('.deadline-format h4');
+  
+  let tempDate = new Date();
+  let tempYear = tempDate.getFullYear();
+  let tempMonth = tempDate.getMonth();
+  let tempDay = tempDate.getDate();
+  // months are ZERO index based;
+  const futureDate = new Date(tempYear, tempMonth, tempDay + 1, 19,0,0);
+  
+  // let futureDate = new Date(2020, 3, 24, 11, 30, 0);
+  
+  const year = futureDate.getFullYear();
+  const hours = futureDate.getHours();
+  const minutes = futureDate.getMinutes();
+  
+  let month = futureDate.getMonth();
+  month = months[month];
+  const weekday = weekdays[futureDate.getDay()];
+  const date = futureDate.getDate();
+  calendario.textContent = `El tiempo termina ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}0pm`;
+  
+  const futureTime = futureDate.getTime();
+  function getRemaindingTime() {
+    const today = new Date().getTime();
+  
+    const t = futureTime - today;
+    // 1s = 1000ms
+    // 1m = 60s
+    // 1hr = 60m
+    // 1d = 24hr
+    // values in miliseconds
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+    // calculate all values
+    let days = t / oneDay;
+    days = Math.floor(days);
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
+  
+    // set values array
+    const values = [days, hours, minutes, seconds];
+    function format(item) {
+      if (item < 10) {
+        return (item = `0${item}`);
+      }
+      return item;
+    }
+  
+    items.forEach(function (item, index) {
+      item.innerHTML = format(values[index]);
+    });
+  
+    if (t < 0) {
+      clearInterval(countdown);
+      deadline.innerHTML = `<h4 class="expired">Lo siento el tiempo expiro!</h4>`;
+    }
+  }
+  // countdown;
+  let countdown = setInterval(getRemaindingTime, 1000);
+  //set initial values
+  getRemaindingTime();
+  
